832f1adfade52580fd28e5298464bcc185b06ac0
孔林 <konglin@aodiansoft.com>
2016-10-18 18:17:28 +0800

    暂存

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -9,12 +9,11 @@
 
 namespace app;
 
+use app\common\ApiHub;
 use app\common\Base\BaseModel;
-use Exception;
 use PhpConsole\Connector;
 use TinyWeb\Application;
 use TinyWeb\ControllerAbstract;
-use TinyWeb\Helper\ApiHelper;
 use TinyWeb\Request;
 use TinyWeb\Response;
 use TinyWeb\Route\RouteApi;
@@ -24,10 +23,10 @@ use TinyWeb\Route\RouteSimple;
 class Bootstrap extends BaseModel
 {
 
-    public static function _debugConsole($data, $tags = null, $ignoreTraceCalls = 0)
+    private static function _debugConsole($data, $tags = null, $ignoreTraceCalls = 0)
     {
         if( !empty($tags) ){
-            $tags = strval($tags) . ' @' . Application::useTimes();
+            $tags = strval($tags) . ':' . Application::useTimes();
         }
         (DEV_MODEL == 'DEBUG') && Connector::getInstance()->getDebugDispatcher()->dispatchDebug($data, $tags, $ignoreTraceCalls);
     }
@@ -42,42 +41,7 @@ class Bootstrap extends BaseModel
         self::_debugConsole($data, $tags, $ignoreTraceCalls);
     }
 
-    public static function apiHub(array $routeInfo, array $params)
-    {
-        $class_name = "\\" . Application::join("\\", [Application::app()->getAppName(), 'api', $routeInfo[0]]);
-        $request = Request::getInstance();
-        $request_method = $request->getMethod();
-        $session_id = $request->getSessionId();
-        if ($request_method == 'HEAD' || $request_method == 'GET' || $request_method == 'OPTIONS' || empty($session_id)) {
-            $log_msg = "CSRF ignore [{$request_method}] this_url:" . $request->getThisUrl();
-            self::debug($log_msg, __METHOD__, __CLASS__, __LINE__);
-        } else {
-            if ( !Application::strCmp($session_id, Application::decrypt($request->getCsrfToken())) ) {
-                $log_msg = "CSRF error [{$request_method}] token:" . $request->getCsrfToken() . ", this_url:" . $request->getThisUrl() . ", referer_url:" . $request->getHttpReferer();
-                self::error($log_msg, __METHOD__, __CLASS__, __LINE__);
-            }
-        }
 
-        try {
-            $json = ApiHelper::api($class_name, $routeInfo[1], $params);
-        } catch (Exception $ex) {
-            $code = $ex->getCode();  // errno为0 或 无error字段 表示没有错误  errno设置为0 会忽略error字段
-            $error = (DEV_MODEL == 'DEBUG') ? ['Exception'=>get_class($ex), 'code' => $ex->getCode(), 'message' => $ex->getMessage(), 'file' => $ex->getFile().' ['.$ex->getLine().']'] : ['code' => $code, 'message' => $ex->getMessage()];
-            $json = ['errno' => $code == 0 ? -1 : $code, 'error' =>$error];
-            while (!empty($ex) && $ex->getPrevious()) {
-                $json['error']['errors'] = isset($json['error']['errors']) ? $json['error']['errors'] : [];
-                $ex = $ex->getPrevious();
-                $json['error']['errors'][] = (DEV_MODEL == 'DEBUG') ? ['Exception'=>get_class($ex), 'code' => $ex->getCode(), 'message' => $ex->getMessage(), 'file' => $ex->getFile().' ['.$ex->getLine().']'] : ['code' => $ex->getCode(), 'message' => $ex->getMessage()];
-            }
-        }
-        $json['apiVersion'] = '1.0';
-        $json_str = isset($params['callback']) && !empty($params['callback']) ? "{$params['callback']}(" . json_encode($json) . ');' : json_encode($json);
-        Response::getInstance()->addHeader('Content-Type: application/json;charset=utf-8', false)->apendBody($json_str, $class_name);
-    }
-
-    public static function ormHub(array $routeInfo, array $params){
-
-    }
 
     /** 在app run 之前, 设置app 命名空间 并 注册路由
      * @param Application $app
@@ -87,10 +51,7 @@ class Bootstrap extends BaseModel
     {
         $app->setAppName('app')
             ->addRoute('api', new RouteApi('api'), function (array $routeInfo, array $params) {
-                call_user_func_array([__CLASS__, 'apiHub'], [$routeInfo, $params]);
-            })
-            ->addRoute('orm', new RouteApi('orm'), function (array $routeInfo, array $params) {
-                call_user_func_array([__CLASS__, 'ormHub'], [$routeInfo, $params]);
+                ApiHub::apiHub($routeInfo, $params);
             })
             ->addRoute('simple', new RouteSimple('m', 'c', 'a'));  // 添加默认路由
         //->addRoute('supervar', new RouteSupervar());   // 添加 r 路由
---
+++ app/common/ApiHub.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/10/18 0018
+ * Time: 18:11
+ */
+
+namespace app\common;
+
+
+use app\common\Base\BaseModel;
+use Exception;
+use TinyWeb\Application;
+use TinyWeb\Helper\ApiHelper;
+use TinyWeb\Request;
+use TinyWeb\Response;
+
+class ApiHub extends BaseModel
+{
+
+    public static function apiHub(array $routeInfo, array $params)
+    {
+        $class_name = "\\" . Application::join("\\", [Application::app()->getAppName(), 'api', $routeInfo[0]]);
+        $request = Request::getInstance();
+        $request_method = $request->getMethod();
+        $session_id = $request->getSessionId();
+        if ($request_method == 'HEAD' || $request_method == 'GET' || $request_method == 'OPTIONS' || empty($session_id)) {
+            $log_msg = "CSRF ignore [{$request_method}] this_url:" . $request->getThisUrl();
+            self::debug($log_msg, __METHOD__, __CLASS__, __LINE__);
+        } else {
+            if ( !Application::strCmp($session_id, Application::decrypt($request->getCsrfToken())) ) {
+                $log_msg = "CSRF error [{$request_method}] token:" . $request->getCsrfToken() . ", this_url:" . $request->getThisUrl() . ", referer_url:" . $request->getHttpReferer();
+                self::error($log_msg, __METHOD__, __CLASS__, __LINE__);
+            }
+        }
+
+        try {
+            $json = ApiHelper::api($class_name, $routeInfo[1], $params);
+        } catch (Exception $ex) {
+            $code = $ex->getCode();  // errno为0 或 无error字段 表示没有错误  errno设置为0 会忽略error字段
+            $error = (DEV_MODEL == 'DEBUG') ? ['Exception'=>get_class($ex), 'code' => $ex->getCode(), 'message' => $ex->getMessage(), 'file' => $ex->getFile().' ['.$ex->getLine().']'] : ['code' => $code, 'message' => $ex->getMessage()];
+            $json = ['errno' => $code == 0 ? -1 : $code, 'error' =>$error];
+            while (!empty($ex) && $ex->getPrevious()) {
+                $json['error']['errors'] = isset($json['error']['errors']) ? $json['error']['errors'] : [];
+                $ex = $ex->getPrevious();
+                $json['error']['errors'][] = (DEV_MODEL == 'DEBUG') ? ['Exception'=>get_class($ex), 'code' => $ex->getCode(), 'message' => $ex->getMessage(), 'file' => $ex->getFile().' ['.$ex->getLine().']'] : ['code' => $ex->getCode(), 'message' => $ex->getMessage()];
+            }
+        }
+        $json['apiVersion'] = '1.0';
+        $json_str = isset($params['callback']) && !empty($params['callback']) ? "{$params['callback']}(" . json_encode($json) . ');' : json_encode($json);
+        Response::getInstance()->addHeader('Content-Type: application/json;charset=utf-8', false)->apendBody($json_str, $class_name);
+    }
+}
