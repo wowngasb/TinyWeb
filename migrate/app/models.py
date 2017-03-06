d865165a3a7098d6ec04b56dee13de1714a884c5
孔林 <konglin@aodiansoft.com>
2017-03-06 10:26:27 +0800

    fix data_seed

--- migrate/app/models.py
+++ migrate/app/models.py
@@ -6,12 +6,13 @@ from app import db, app
 
 Base = db.Model
 
+
 class VlssApp(Base):
     __tablename__ = 'vlss_app'
 
     vlss_id = Column(Integer, primary_key=True)   #虚拟演播厅自增id
     login_name = Column(String(16), nullable=False, unique=True)   #用户管理后台登录名
-    password = Column(String(16), nullable=False)   #用户管理后台登录名
+    password = Column(String(32), nullable=False)   #用户管理后台登录名
     access_id = Column(String(64), nullable=False)   #奥点云access_id
     access_key = Column(String(64), nullable=False)   #奥点云access_key
     aodian_uin = Column(Integer, nullable=False, index=True)   #奥点云 uin
@@ -19,8 +20,10 @@ class VlssApp(Base):
     dms_pub_key = Column(String(64), nullable=False)   #DMS pub_key
     dms_s_key = Column(String(64), nullable=False)   #DMS s_key
     lcps_host = Column(String(128), nullable=False, index=True)   #导播台域名  不带http://前缀 和 结尾/
+    active_group_id = Column(Integer, nullable=False, server_default=text("'0'"))   #激活的场景组id
+    active_template_id = Column(Integer, nullable=False, server_default=text("'0'"))   #激活的场景模版id
     state = Column(SmallInteger, nullable=False, server_default=text("'0'"))   #1正常，2冻结，9删除
-    last_login_id = Column(String(32), nullable=False, server_default=text("''"))   #用户上次登录ip
+    last_login_ip = Column(String(32), nullable=False, server_default=text("''"))   #用户上次登录ip
     login_count = Column(Integer, nullable=False, server_default=text("'0'"))   #用户管理后台登录次数 登陆一次+1
     create_time = Column(DateTime, nullable=False)   #记录创建时间
     uptime = Column(TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))   #更新时间
@@ -45,7 +48,7 @@ class VlssSceneItem(Base):
     group_id = Column(Integer, nullable=False, index=True, server_default=text("'0'"))   #所属场景组id
     scene_name = Column(String(32), nullable=False)   #场景名称
     scene_config = Column(Text, nullable=False)   #场景配置 格式为 json 字符串
-    scene_type = Column(String(16), nullable=False, server_default=text("'0'"))   #场景类型1弹幕;2预告;3封面;4投票;5片尾;6记分牌;7弹幕2;8LOGO;9字幕;10通用场景;足球场景
+    scene_type = Column(String(16), nullable=False, server_default=text("''"))   #场景类型
     scene_sort = Column(Integer, nullable=False, server_default=text("'10'"))   #场景叠加排序
     state = Column(SmallInteger, nullable=False, server_default=text("'0'"))   #1正常,2隐藏,9删除
     create_time = Column(DateTime, nullable=False)   #记录创建时间
--- migrate/db_seed.py
+++ migrate/db_seed.py
@@ -1,21 +1,24 @@
 # coding: utf-8
 from app import db, models, md5key
+import datetime
 
 vlss_list = [
     ('demo', 'demo', '111576745758', 'fVYGq1S0gnrvoxZv77msq577jx7MQq3n', 13830, 'sub_eae37e48dab5f305516d07788eaaea60', 'pub_5bfb7a0ced7adb2ce454575747762679', 's_ceb80d29276f78653df081e5a9f0ac76', '123.8887.lcps.aodianyun.com'),
 ]
 
 [db.session.add(
-    models.VlssApp(login_name=item[0],
-    password=md5key(item[1]),
-    access_id=item[2],
-    access_key=item[3],
-    aodian_uin=item[4],
-    dms_sub_key=item[5],
-    dms_pub_key=item[6],
-    dms_s_key=item[7],
-    lcps_host=item[8],
-    create_time=date()
-    state=1)) for item in vlss_list if not models.VlssApp.query.filter_by(login_name=item[0]).first()]
+    models.VlssApp(
+        login_name=item[0],
+        password=md5key(item[1]),
+        access_id=item[2],
+        access_key=item[3],
+        aodian_uin=item[4],
+        dms_sub_key=item[5],
+        dms_pub_key=item[6],
+        dms_s_key=item[7],
+        lcps_host=item[8],
+        create_time=datetime.datetime.now(),
+        state=1)
+    ) for item in vlss_list if not models.VlssApp.query.filter_by(login_name=item[0]).first()]
 
 db.session.commit()
