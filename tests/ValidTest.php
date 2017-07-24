f6f78ff40ff3bfb92d795b681a94704022380158
孔林 <konglin@aodiansoft.com>
2017-07-24 18:20:38 +0800

    测试脚本

---
+++ tests/ValidTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/7/24 0024
+ * Time: 17:52
+ */
+class ValidTest  extends PHPUnit_Framework_TestCase
+{
+    public function testNothing()
+    {
+    }
+
+
+}
